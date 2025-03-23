cb3fd0f2eb5a3f6eae21ed94bc9113de69745101
pshilakari <pragya.shilakari@gmail.com>
2025-03-23 18:46:28 +0000

    Updated to test 2nd TMP117 sensor at 0x49

--- src/SmartMonitoringAndSafetySystem.cpp
+++ src/SmartMonitoringAndSafetySystem.cpp
@@ -11,6 +11,10 @@
 #include <cmath>
 
 #include "gpioevent.h"
+#include <mutex>
+
+// Mutex to protect the use of single I2C bus shared by multiple sensors
+static std::mutex i2c_mutex; 
 
 class TMP117TemperatureSensor : public GPIOPin::GPIOEventCallbackInterface {
 
@@ -18,7 +22,8 @@ class TMP117TemperatureSensor : public GPIOPin::GPIOEventCallbackInterface {
 	#define GPIO_CHIP "/dev/gpiochip0"  // Raspberry Pi 5 GPIO controller
 	#define ALERT_PIN 17                // GPIO17 (TMP117 ALERT pin)
 	#define I2C_BUS "/dev/i2c-1"         // I2C bus for TMP117
-	#define TMP117_ADDR 0x48             // Default I2C address for TMP117
+	#define TMP117_ADDR1 0x48             // Default I2C address for TMP117
+	#define TMP117_ADDR2 0x49             // Default I2C address for TMP117
 	#define TMP117_TEMP_REG 0x00         // Temperature register address
 
 	virtual void hasEvent(gpiod_line_event& e) override {
@@ -43,10 +48,11 @@ class TMP117TemperatureSensor : public GPIOPin::GPIOEventCallbackInterface {
 		printf("Unkown event\n");
 	    }
 	}
-/**
- * Function to read temperature from TMP117 over I2C
- */
-double readTemperature() {
+	/**
+	 * Function to read temperature from TMP117 over I2C
+	 */
+	double readTemperature() {
+		std::lock_guard<std::mutex> lock(i2c_mutex);
 	/*
 	* Step#1: Opening the I2C Bus Device File:
 	* --------------------------------
@@ -105,15 +111,29 @@ double readTemperature() {
 	*         // Handle error
 	*     }
 	*/
-    if (ioctl(fd, I2C_SLAVE, TMP117_ADDR) < 0) {
-        printf("Error: Failed to set I2C address! %s\n", strerror(errno));
-        close(fd);
-        return NAN;
-    }
+	if (ioctl(fd, I2C_SLAVE, TMP117_ADDR2) < 0) {
+		printf("Error: Failed to set I2C address! %s\n", strerror(errno));
+		close(fd);
+		return NAN;
+	}
+	/*if (sensor_id == 1){
+		if (ioctl(fd, I2C_SLAVE, TMP117_ADDR1) < 0) {
+			printf("Error: Failed to set I2C address! %s\n", strerror(errno));
+			close(fd);
+			return NAN;
+		}
+	}
+	else if (sensor_id == 0){
+		if (ioctl(fd, I2C_SLAVE, TMP117_ADDR2) < 0) {
+			printf("Error: Failed to set I2C address! %s\n", strerror(errno));
+			close(fd);
+			return NAN;
+		}
+	}*/
 	/**
 	 * recommended to add a short delay after interrupt to ensure the TMP117 data is stable
 	 */
-	usleep(2000); 
+	usleep(5000); //2000 was giving Error: Failed to write to TMP117! Remote I/O error
 	
 	/**
 	*
@@ -202,24 +222,38 @@ double readTemperature() {
 	 */
     int16_t rawTemp = (buffer[0] << 8) | buffer[1];
     return rawTemp * 0.0078125;  // Conversion formula from TMP117 datasheet
-}
+	}
+	/*
+	public:
+
+	TMP117TemperatureSensor(int sensor_id = 0){
+		this->sensor_id = sensor_id;
+	}
+	private:
+	int sensor_id = 0;
+	*/
 };
 
 int main(int argc, char *argv[]) {
 	fprintf(stderr,"Press any key to stop.\n");
-	TMP117TemperatureSensor t1; //, t2;
-	GPIOPin gpiopin17; //, gpiopin27;
+	TMP117TemperatureSensor t1;//, *t2;
+	/*
+	TMP117TemperatureSensor *t1, *t2;
+	t1 = new TMP117TemperatureSensor(0);
+	t2 = new TMP117TemperatureSensor(1);
+	*/
+	GPIOPin gpiopin17, gpiopin27;
 
-	gpiopin17.registerCallback(&t1);
-	const int gpioPinNo17 = 17;
-	gpiopin17.start(gpioPinNo17);
+	//gpiopin17.registerCallback(&t2);
+	//const int gpioPinNo17 = 17;
+	//gpiopin17.start(gpioPinNo17);
 
-	//gpiopin27.registerCallback(&t2);
-	//const int gpioPinNo27 = 27;
-	//gpiopin27.start(gpioPinNo27);
+	gpiopin27.registerCallback(&t1);
+	const int gpioPinNo27 = 27;
+	gpiopin27.start(gpioPinNo27);
 
 	getchar();
     gpiopin17.stop();
-	//gpiopin27.stop();
+	gpiopin27.stop();
 	return 0;
 }
